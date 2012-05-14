63652139b93adca817247bf68af6ca93f7f1b2a3
Mathias Fussenegger <f.mathias@zignar.net>
2012-05-14 23:19:06 +0200

    implemented convert function.
    So far without template support

--- baanprint/bwprint.py
+++ baanprint/bwprint.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 import logging
+import tempfile
+from time import time, sleep
 
 from baanprint import config
+
+from subprocess import check_call as call
 
 from argh import ArghParser, command
 from yapsy.PluginManager import PluginManager, IPlugin
@@ -32,7 +37,53 @@ def convert(inputf, outputf, template=None):
     :param outpuf: path to the pdf file that will be created
     :param template: 1 page pdf file, will be overlayed onto each page
     """
-    pass
+    doc = BwDocument(inputf)
+    bpf, token = doc.dump()
+
+    args = config.bwprint_exe + ['/p', '/r', bpf]
+    call(args)
+    if os.path.exists(bpf):
+        os.remove(bpf)
+
+    pdf = get_pdf_file(token)
+    if pdf:
+        shutil.move(pdf, outputf)
+
+
+def get_pdf_file(token):
+    absfilename = find_pdf_file(token)
+    assert isinstance(absfilename, str)
+    wait_until_file_is_written(absfilename)
+    return absfilename
+
+
+def wait_until_file_is_written(filename):
+    """sleep until the file doesn't increase in size
+
+    cups created the file immediately, but keeps writting to it.
+    :param filename: file whose size is checked
+    """
+
+    while True:
+        lastsize = os.path.getsize(filename)
+        sleep(0.1)
+        if os.path.getsize(filename) == lastsize:
+            break
+
+
+def find_pdf_file(token):
+    """iterates the printers output directory for the pdf
+
+    :param token: token that the filename should contain.
+    :returns the absolute path to the pdf file.
+    """
+
+    # it might take a while until the file is created
+    for i in range(20):
+        for filename in os.listdir(config.printer_output):
+            if filename.startswith(token):
+                return os.path.join(config.printer_output, filename)
+        sleep(0.1)
 
 
 @command
@@ -84,6 +135,7 @@ class BwDocument(object):
         self.creator = None
         self.doc_type = None
         self.pages = {1: ''}
+        self.printer = None
         self.page_size = config.default_page_size
         self.read_file(bpf_path)
 
@@ -105,6 +157,34 @@ class BwDocument(object):
                     self.pages[page] = ''
 
                 self.pages[page] += line + os.linesep
+
+    def dump(self, printer=None):
+        """writes the in-memory bp-file into a temporary file on disk
+
+        :param printer: printer line that is added in the first line of the file.
+                        In case the printer is null, config.pdf_printer is used.
+
+        The printer line is read by bwprint.exe and is required to specify the
+        printer which should be used. In addition, settings like page margins,
+        page width and size are also specified.
+
+        A token is added into the printer line. This token is used to identify
+        the file that is printed using the pdf printer.
+        """
+
+        if not printer:
+            printer = self.printer or config.pdf_printer
+
+        _, output = tempfile.mkstemp('.bpf', dir=os.curdir)
+        #output = os.path.join(os.curdir, 'test.bpf')
+        token = str(time()).replace('.', '_')
+
+        with open(output, 'w', encoding='latin1') as fd:
+            fd.write(printer.format(token) + '\n')
+            for page in self.pages:
+                fd.writelines(self.pages[page])
+
+        return output, token
 
 
 def main():
