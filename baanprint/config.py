90a902775c004ed6d4e3773e8dbdb16828a6edeb
Mathias Fussenegger <f.mathias@zignar.net>
2012-05-14 23:18:50 +0200

    add required settings to config file

--- baanprint/config.py
+++ baanprint/config.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 default_page_size = 66
+bwprint_exe = ['wine',
+               '/home/USER/.wine/drive_c/Program Files/Baan/Baan Windows/bin/bwprint.exe']
+
+pdf_printer = '@ -p "Virtual PDF Printer" -c 2 -f 1 -t 9999999 -l 66 -m 4 -L -a 1 -r {0} -w 132 -s 9 -d P -B "PDFCREATOR"@"'
+printer_output = '/var/spool/cups-pdf/USER/'
+
+# windows settings
+# bwprint_exe = ['C:\Program Files (x86)\Infor\BW\ERPLN\bin\bwprint.exe']
+# printer_output = 'c:\Temp\pdf'
